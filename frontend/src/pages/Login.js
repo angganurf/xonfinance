dd1de3a63bef6900ba01c83a137fdb9882372517
angganurfaisal <angganurfaizal234@gmail.com>
2025-11-25 18:25:20 +0000

    auto-commit for d0496abd-edfa-483a-a1dc-d737c19e7dd4

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,6 +6,7 @@ import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../components/ui/select';
 import { toast } from 'sonner';
 import { Building2 } from 'lucide-react';
 
