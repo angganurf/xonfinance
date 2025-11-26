860451260e41633bc2d773a0f248138ccf32a3a3
angganurfaisal <angganurfaizal234@gmail.com>
2025-11-26 15:21:11 +0000

    auto-commit for 88a79b7a-29d6-46bb-a0c9-fee8607369ae

--- frontend/src/pages/drafter/Dashboard.js
+++ frontend/src/pages/drafter/Dashboard.js
@@ -5,7 +5,6 @@ import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
 import { Label } from '../../components/ui/label';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '../../components/ui/dialog';
-import { Checkbox } from '../../components/ui/checkbox';
 import { toast } from 'sonner';
 import { Plus, CheckCircle, Circle, Trash2, Clock, CheckCheck } from 'lucide-react';
 import api from '../../utils/api';
