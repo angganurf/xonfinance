3dc6b1ca8efb1c516a6f2fc14f6d2def9557cffd
angganurfaisal <angganurfaizal234@gmail.com>
2025-11-26 05:30:16 +0000

    auto-commit for a1a5dea9-36ee-4d95-81c4-446b46def3b5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import EmployeeTasks from './pages/employee/Tasks';
 import AdminDashboard from './pages/admin/Dashboard';
 import AdminMembers from './pages/admin/Members';
 import AdminInventory from './pages/admin/Inventory';
+import AdminSettings from './pages/admin/Settings';
 import Settings from './pages/Settings';
 import './App.css';
 
